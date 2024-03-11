baa4dee842753d391ec20ee0b44c5c0e66b8cd95
Ashok j <ashok.jalondhara56@gmail.com>
2024-03-11 13:30:14 +0530

    key and token set

--- cricket/app/Http/Middleware/EnsureTokenIsValid.php
+++ cricket/app/Http/Middleware/EnsureTokenIsValid.php
@@ -15,10 +15,10 @@ class EnsureTokenIsValid
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // $cricktownHost = $request->header('X-Crictown28Infotech-Host');
-        // $cricktownKey = $request->header('X-Crictown28Infotech-Key');
-        $cricktownHost = 'cricktown.28infotech.com';
-        $cricktownKey = '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183';
+        $cricktownHost = $request->header('X-Crictown28Infotech-Host');
+        $cricktownKey = $request->header('X-Crictown28Infotech-Key');
+        // $cricktownHost = 'cricktown.28infotech.com';
+        // $cricktownKey = '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183';
 
         if ($cricktownHost !== 'cricktown.28infotech.com' || $cricktownKey !== '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183') {
             abort(403, 'Authorization flailed');
