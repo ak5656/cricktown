f691b73be443e92937c50e9f7eadd6f6760ba94d
Ashok j <ashok.jalondhara56@gmail.com>
2024-03-12 10:54:54 +0530

    bug clear

--- cricket/app/Http/Middleware/EnsureTokenIsValid.php
+++ cricket/app/Http/Middleware/EnsureTokenIsValid.php
@@ -18,7 +18,7 @@ class EnsureTokenIsValid
         $cricktownHost = $request->header('X-Crictown28Infotech-Host');
         $cricktownKey = $request->header('X-Crictown28Infotech-Key');
         $cricktownHost = 'cricktown.28infotech.com';
-        $cricktownKey = '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183';
+        // $cricktownKey = '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183';
         $keyCollection = [
             '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183', // current token
             'f4e78le451dmwlfha1flr732f8r437l54913lej1ls154m4g48', // next token
