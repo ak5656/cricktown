6cbeca702ee4c977731cdd81ed0129d32d9fc21c
Ashok j <ashok.jalondhara56@gmail.com>
2024-03-12 10:49:50 +0530

    multipule token

--- cricket/app/Http/Middleware/EnsureTokenIsValid.php
+++ cricket/app/Http/Middleware/EnsureTokenIsValid.php
@@ -17,10 +17,14 @@ class EnsureTokenIsValid
     {
         $cricktownHost = $request->header('X-Crictown28Infotech-Host');
         $cricktownKey = $request->header('X-Crictown28Infotech-Key');
-        // $cricktownHost = 'cricktown.28infotech.com';
-        // $cricktownKey = '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183';
+        $cricktownHost = 'cricktown.28infotech.com';
+        $cricktownKey = '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183';
+        $keyCollection = [
+            '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183', // current token
+            'f4e78le451dmwlfha1flr732f8r437l54913lej1ls154m4g48', // next token
+        ];
 
-        if ($cricktownHost !== 'cricktown.28infotech.com' || $cricktownKey !== '9f4h197fk6clwpqlx6f469x17cp5v42l568vmej4lsn469x183') {
+        if ($cricktownHost !== 'cricktown.28infotech.com' || !in_array($cricktownKey, $keyCollection)) {
             abort(403, 'Authorization flailed');
         }
         return $next($request);
